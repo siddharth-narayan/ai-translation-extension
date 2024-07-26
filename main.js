ff2d3b97639aa21fa99d086291f97192d375499b
Siddharth <siddharth.060605@gmail.com>
2024-07-26 18:03:29 -0400

    Translate text nodes properly with [SEP] token

--- main.js
+++ main.js
@@ -2,48 +2,23 @@ document.body.style.border = "5px solid red";
 
 main()
 
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 async function main() {
     while (true) {
-        let textNodes = nativeTreeWalker()
-        for (let i = 0; i < textNodes.length; i++) {
-            let node = textNodes[i]
-            let text = node.nodeValue.trimStart().trimEnd()
-            let parentTagName = node.parentElement?.tagName
-            let allowedElementTypes = ["A", "P", "H1", "H2", "H3", "H4"]
-        
-            let disallowedElementType = !allowedElementTypes.includes(parentTagName)
-            let emptyString = text === ''
-            let previouslyTranslated = node.parentElement.classList.contains('ai-translated')
-            let tranlationInProgress = node.parentElement.classList.contains('ai-translating')
-            if (disallowedElementType || emptyString || previouslyTranslated || tranlationInProgress) {
-                continue
-            }
-        
-            console.log("Text: " + text + " Tag name: " + parentTagName)
-        
-            let url = "http://localhost:5000/translate?" + new URLSearchParams({text: text}).toString()
-            
-            node.parentElement.classList.add(['ai-translating'])
-            let response = await fetch(url)
-            if (response.ok) {
-                let text = await response.text()
-                console.log(text)
-                node.nodeValue = text
-                node.parentElement.classList.remove(['ai-translating'])
-                node.parentElement.classList.add(['ai-translated'])
-            }
-        }
-
-        await timer(5000)
+        let el = document.getElementsByTagName('p')[0]
+        translateElementTextNodes(el)
+        await delay(5000)
         console.log("loop")
     }
 }
 
-const timer = ms => new Promise(res => setTimeout(res, ms))
 
-function nativeTreeWalker() {
+function nativeTreeWalker(element) {
     var walker = document.createTreeWalker(
-        document.body, 
+        element, 
         NodeFilter.SHOW_TEXT,
     );
 
@@ -55,4 +30,37 @@ function nativeTreeWalker() {
     }
 
     return textNodes
+}
+
+async function translateElementTextNodes(element) {
+    let previouslyTranslated = element.classList.contains('ai-translated')
+    let tranlationInProgress = element.classList.contains('ai-translating')
+    if (previouslyTranslated || tranlationInProgress) {
+        return
+    }
+
+    let text = ''
+    let translation
+
+    let textNodes = nativeTreeWalker(element)
+    for (let i = 0; i < textNodes.length; i++) {
+        let node = textNodes[i]
+        text = text.concat(node.nodeValue.trimStart().trimEnd(), ' [SEP]')
+    }
+
+    console.log("Text: " + text)
+    
+    let url = "http://localhost:5000/translate?" + new URLSearchParams({text: text}).toString()
+    
+    element.classList.add(['ai-translating'])
+    let response = await fetch(url)
+    if (response.ok) {
+        translation = await response.text()
+        console.log(translation)
+        element.classList.remove(['ai-translating'])
+        element.classList.add(['ai-translated'])
+    }
+
+    translation.split(' [SEQ]').forEach((string, index) => textNodes[index] = string)
+
 }
